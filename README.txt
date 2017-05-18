42b70febab49d9ad24396a95c8cd0da73096dc1e
DForbes <damianoforbes@gmail.com>
2017-05-17 20:32:07 -0400

    perl based automation
    perl based on python

---
+++ README.txt
@@ -0,0 +1,19 @@
+			Downloading/Using modules (PIP): 
+CPAN - http://www.cpan.org/modules/INSTALL.html
+				  http://search.cpan.org/search?query=open%20url&mode=all
+PPM (Getting Modules)- https://www.ibm.com/developerworks/aix/library/au-perltkmodule/
+Automation: http://www.perlmonks.org/?node_id=1141108
+GUI broken? (Tk):http://www.wellho.net/resources/ex.php4?item=p307/tk1
+	http://search.cpan.org/~ni-s/Tk-804.027/pod/UserGuide.pod
+GUI(Tkx): http://docs.activestate.com/activeperl/5.8/lib/Tkx/Tutorial.html
+launch broswer: http://stackoverflow.com/questions/8867262/how-do-i-launch-the-default-web-browser-in-perl-on-any-operating-system
+
+			Seleinium: 
+Module Doc: https://metacpan.org/pod/Selenium::Remote::Driver
+Error with Seleinum: http://techqa.info/programming/question/41251445/perl-and-selenium::remote::driver
+Automation: http://blog.unicsolution.com/2013/08/automated-web-testing-with-perl-and.html
+
+Perl Time:https://www.tutorialspoint.com/perl/perl_sleep.htm
+
+Youtube:
+https://www.youtube.com/watch?v=1UYBAn69Qrk
---
+++ patientPerl.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl -w 
+
+# Based on - Patient Portal Enrollments implemented in Python
+
+use 5.18.0;
+use warnings;
+use Browser::Open qw (open_browser);
+use IO::File;
+use Tkx;
+use Selenium::Remote::Driver;
+
+#say "Hello World";
+#my $url = 'http://www.google.com/';
+#open_browser($url);
+
+# Create GUI window
+my $mw = Tkx::widget->new(".");
+$mw->g_wm_title("Automation");
+$mw->g_wm_minsize(400,100);
+
+#
+#my $label = $mw->new_label(-text=>"LABEL1")->pack;
+
+# Create Button
+#my $button;
+#$button = $mw->new_button(
+#    -text => "Login", -command=>sub{},-command => sub { $button->m_configure(-text => "UB Login",);
+#        Tkx::after(5000, sub{$mw->portal});
+#    },
+#);
+
+# Button Creations
+my $button;
+my $button2;
+$button = $mw->new_button(-text => "Web Login", -command => \&ubPortal);
+$button2 = $mw->new_button(-text => "Cortext Login", -command => \&cortextPWDRest);
+
+# Button postions
+$button->g_pack(-padx => 10,-pady => 10,);
+$button2->g_pack(-padx => 10,-pady => 10,);
+
+Tkx::MainLoop();
+
+
+# Functions
+sub ubPortal{
+    # Set Chrome as Default
+    my $url = 'https://shibboleth.buffalo.edu/idp/Authn/Stateless';
+    open_browser($url)   
+}
+
+sub cortextPWDRest{
+    # Set Chrome as driver for browser/Launch with WebDriver
+    my $chrome_driver = Selenium::Remote::Driver->new;
+    
+    # Webpage Address   
+    my $url = 'https://admin.cortext.com/admin-center/#login';
+    
+    # TODO - get admin name/credentials
+    my $admin = '<USERNAME>';
+    my $password = '<PASSWORD>';
+    
+    # TODO - get webpage
+    $chrome_driver->get($url);
+    
+    # TODO: Admin Username, enter info and click enter
+    # Username
+    $chrome_driver->find_element_by_xpath("//input[\@id='username']");
+    $chrome_driver->send_keys($admin);
+    # Password
+    $chrome_driver->find_element_by_id("password");
+    $chrome_driver->send_keys($password);
+    # Login
+    $chrome_driver->find_element_by_xpath("//*[\@id='tabUsers']/a")->click();    
+    
+    # Sleep
+    
+    # TODO: Search Users
+    $chrome_driver->find_element_by_xpath("//*[\@id='searchField']"); 
+    
+    
+    # TODO - launch application (test with Word/Office)
+}
+
+
+
